ec591bd9e2d69e73dfc958d43e1844760bf85d20
andywu91 <andywu91@foxmail.com>
2023-09-24 21:57:01 +0800

    upgradle build.gradle in app module to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.wuliang.xapkinstaller"
+        minSdkVersion(14)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        val release by getting {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildToolsVersion = "28.0.3"
+}
+
+dependencies {
+//    implementation(fileTree(dir: "libs", include: ["*.jar"]))
+
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+    implementation(project(":lib"))
+    implementation("com.liulishuo.okdownload:okdownload:1.0.7")
+
+    testImplementation("junit:junit:4.12")
+}
